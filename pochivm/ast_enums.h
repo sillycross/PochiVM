11d66c72ba756d040cd631f9f17eb36ab4c3c970
Haoran Xu <haoranxu510@gmail.com>
2020-09-28 16:07:13 -0700

    [FastInterp] AstBlock, no test yet

--- pochivm/ast_enums.h
+++ pochivm/ast_enums.h
@@ -453,4 +453,15 @@ enum class FILogicalOperatorLv2TreeMask
     X_END_OF_ENUM = FILogicalOperatorTree<2>::x_max
 };
 
+const int x_fastinterp_block_max_stmts = 11;
+enum class FIBlockNumStatements
+{
+    X_END_OF_ENUM = x_fastinterp_block_max_stmts + 1
+};
+
+enum class FIBlockMayCFRMask
+{
+    X_END_OF_ENUM = 1 << static_cast<int>(FIBlockNumStatements::X_END_OF_ENUM)
+};
+
 }   // namespace PochiVM
