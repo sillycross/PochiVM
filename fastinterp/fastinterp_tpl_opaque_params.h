258d49b47a68c32f6d1c0611f5fa4d86114d9233
Haoran Xu <haoranxu510@gmail.com>
2020-10-04 03:20:04 -0700

    add some comments

--- fastinterp/fastinterp_tpl_opaque_params.h
+++ fastinterp/fastinterp_tpl_opaque_params.h
@@ -5,6 +5,32 @@
 namespace PochiVM
 {
 
+// We keep a small number of temporary values directly in register, instead of on the stack, for performance reasons.
+// Note that here we are taking advantage of the fact that GHC calling convention only
+// differentiates integral (bit-type) and floating point parameters.
+// The actual width and signness of the parameters, and the relative order *between* integer and floating point parameters
+// does not affect ABI. As an example, consider the following:
+//    void f(int b, float a) { ... }
+//    void g(uint64_t a, double b) { reinterpret_cast<void(*)(uint64_t, double)>(f)(a, b); }
+//    void h() { float a = 1.2; int b = 3; reinterpret_cast<void(*)(float, int)>(g)(a, b); }
+//
+// Despite the call to 'f' and 'g' completely mismatch its C prototype declaration,
+// function 'f' can actually see the same value of 'a' and 'b' as in 'h', since all three functions actually have the same ABIs.
+// (the 'intermediate function' 'g' having the longest possible type 'uint64_t' and 'double' is important:
+// otherwise if 'g' decides to temporarily push its parameters to stack, it could push and pop a shorter value than actual).
+//
+// This property allows us to easily pass an opaque 'stack' of temporary values to an operator's next continuation,
+// without needing to know the actual types of the parameters, since only how many of them exist, and how many of them are
+// floating points matters. So we won't have an exponentially exploding number of boilerplates.
+//
+// The callee can retrieve the top value from the stack of temporary values by doing something like
+//   template<typename... T> f(T... opaque, int value)    // get top of the integer stack, it must be actually an 'int' of course
+// or 'push' new values to be passed to its continuation by doing something like
+//   DEFINE_BOILERPLATE_FNPTR_PLACEHOLDER_0(void(*)(T..., int))
+//   BOILERPLATE_FNPTR_PLACEHOLDER_0(opaque..., value)
+// Note that this works fine even if 'T...' contains floats, since the relative order *between* integer
+// and floating point does not affect ABI. All the thing that matters is the relative order within all integers and within all floats.
+//
 const int x_fastinterp_max_integral_params = 3;
 const int x_fastinterp_max_floating_point_params = 3;
 
